4a3941e7bf7e49e4ed5e285edf38e2fb753dff36
Victor Sandin <victor.sandin@tabby.ai>
2025-09-11 14:04:19 +0200

    add keybindings for moving between buffers

--- lua/mario/default.lua
+++ lua/mario/default.lua
@@ -40,3 +40,8 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.api.nvim_set_option_value("linebreak", true, {scope = "local"})
     end
 })
+
+vim.keymap.set("n", "<S-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<S-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "<S-j>", "<C-w>j", { desc = "Move to split below" })
+vim.keymap.set("n", "<S-k>", "<C-w>k", { desc = "Move to split above" })
